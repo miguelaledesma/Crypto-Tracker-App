b7a189b84615a8bee4b6a2e245a0a2b5ced1f1f4
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-06 16:30:35 -0700

    working on navigating from page to page from the click of the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { AppBar } from "@mui/material";
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import {  useNavigate } from "react-router";
+
+
+
 
 const Header = () => {
+
+    const history = useNavigate()
+
+
     return(
-        <AppBar color = 'transparent' >
-            
+        <AppBar color = 'transparent' position = 'static'>
+            <Container>
+                <Toolbar>
+                    <Typography 
+                    onClick = {() => history('/')} 
+                    className = "title" 
+                    style = {{fontFamily: 'Montserrat', fontWeight: 'bold' }}
+                    >Crypto Tracker</Typography>
+
+                    <Select variant = "outlined"
+                     style = {{
+                         width: 100, 
+                         height: 40,
+                         margin: 15
+                     }}
+                     >
+                        <MenuItem value = {'USD'}>USD</MenuItem>
+                        <MenuItem value = {'EUR'}>EUR</MenuItem>
+                    </Select>
+                </Toolbar>
+            </Container>
+             
         </AppBar>
     )
 }
