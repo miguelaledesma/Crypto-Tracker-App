f07f7e0a284ab4e296e1b54420ec599dc72268d2
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-05 19:01:43 -0700

    adding components and routes to my app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Route } from "react-router-dom"
+import Header from "./components/Header";
 import './App.css';
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
 
 function App() {
   return (
-    <div className="App">
-      Hello World 
+    <BrowserRouter>
+    <div>
+      <Header /> 
+      <Route  path= '/' component ={Homepage} /> 
+      <Route  path= '/coins/:id' component ={CoinPage} /> 
+      Hello 
     </div>
+    </BrowserRouter>
+
   );
 }
 
