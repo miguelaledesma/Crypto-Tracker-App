dee1ab3846254686263cf5bd476734c4f98f90c2
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-29 19:08:00 -0700

    ip

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import { HistoricalChart } from "../config/api";
+import { CircularProgress } from "@mui/material";
 
-const CoinInfo = () => {
+const CoinInfo = ({coin}) => {
+    const [historicalData, setHistoricalData] = useState()
+    const [days, setDays] = useState(1)
+    const { currency, symbol } = CryptoState()
+
+    const fetchHistoricalData = async () => {
+        const {data} = axios.get(HistoricalChart(coin.id, days, currency))
+        setHistoricalData(data.prices)
+    }
+
+    useEffect(() => {
+        fetchHistoricalData()
+    },[currency, days])
+
+
     return (
-        <div> coin info is working </div>
+        <div className = 'chartContainer'>
+             {
+              !historicalData ? (
+                <CircularProgress size = {150} style ={{color: 'blue'}} thickness = {1}/>
+            ): (<></>)
+             }
+
+        </div>
     )
 }
 
