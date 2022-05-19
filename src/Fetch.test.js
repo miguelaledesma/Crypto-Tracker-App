911ccfd06fbf77d468ab83ebf39df198ecbf0598
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-05-19 12:57:59 -0700

    working on incorporating tests for api fetch and gameplanning crypto historical chart

---
+++ src/Fetch.test.js
@@ -0,0 +1,22 @@
+import {render, screen} from "@testing-library/react"
+import CoinsTable from "./components/CoinsTable";
+import { rest } from "msw";
+import {setupServer} from "msw/node"
+import {SingleCoin} from './config/api'
+import App from "./App";
+
+// const response = rest.get(SingleCoin , (req, res, ctx)  => {
+//     return  res(ctx.json([{id: 'bitcoin', symbol: 'btc', name: 'Bitcoin'}]))
+// } )
+
+// const handlers = [response]
+
+// const server = new setupServer()
+
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
+
+// test('renders root component', async  () => {
+
+// })
