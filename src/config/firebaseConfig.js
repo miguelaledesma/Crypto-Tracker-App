9750c498f93ce5fe3506c0c5d67a28bbce07aaf9
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-06-21 10:37:03 -0700

    pushing some minor changes to auth

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ttyCBy0t4s534pV8xGcfo3fY917shas",
+  authDomain: "pocketcrypto-6420e.firebaseapp.com",
+  projectId: "pocketcrypto-6420e",
+  storageBucket: "pocketcrypto-6420e.appspot.com",
+  messagingSenderId: "750947945331",
+  appId: "1:750947945331:web:c7c896ab2011eb684b5306",
+};
+
+export default firebaseConfig;
