f80bd642d2f12ad9a2b4d651830c3aa4a3317ccc
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-17 19:24:26 -0700

    backend news api working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
-
-app.get("/api", (req, res) => {
-  res.json();
+const today = Date.now();
+app.get("/news", async (req, res) => {
+  const articles = await axios.get(
+    `https://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
+  );
+  res.send({ articles: articles.data.articles });
 });
 
 app.listen(3001, () => {
