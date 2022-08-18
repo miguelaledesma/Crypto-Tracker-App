13b7b565eda50a3cb33a2f06104a549ca9bdda3d
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-17 19:45:52 -0700

    news api is wired up in the backend now just need to connect it to the front end

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const axios = require("axios");
 const app = express();
 const today = Date.now();
 app.get("/news", async (req, res) => {
-  const articles = await axios.get(
-    `https://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
-  );
-  res.send({ articles: articles.data.articles });
+  try {
+    const articles = await axios.get(
+      `https://newsapi.org/v2/everything?q=bitcoin&from=2022-07-18&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
+    );
+    console.log(res);
+    res.send({ articles: articles.data.articles });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3001, () => {
