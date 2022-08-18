8b4e58f5d410e50277781503e4f0c44e6b36dad6
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-17 20:03:50 -0700

    added cors to backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const today = Date.now();
-app.get("/news", async (req, res) => {
+app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
       `https://newsapi.org/v2/everything?q=bitcoin&from=2022-07-18&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
     );
-    console.log(res);
+
     res.send({ articles: articles.data.articles });
+    console.log(articles);
   } catch (err) {
     console.log(err);
   }
