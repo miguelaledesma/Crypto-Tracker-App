30328666de81dc9659bc214b6d9e7732f65f12a3
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-19 13:58:07 -0700

    heroku build

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const today = Date.now();
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -11,7 +13,7 @@ app.get("/", (req, res) => {
 app.get("/news", cors(), async (req, res) => {
   try {
     const articles = await axios.get(
-      `https://newsapi.org/v2/everything?q=bitcoin&from=2022-07-18&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
+      `https://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
     );
 
     res.send({ articles: articles.data.articles });
