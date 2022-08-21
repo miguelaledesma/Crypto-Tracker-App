d6601ff473bef2e673d4622fefb5871ad69d2833
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-08-21 12:19:56 -0700

    added loading to news article page

--- src/Pages/News.js
+++ src/Pages/News.js
@@ -5,26 +5,8 @@ import Container from "@mui/material/Container";
 import { Pagination, LinearProgress, Typography } from "@mui/material";
 
 const CryptoNews = () => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState();
   const [page, setPage] = useState(1);
-
-  const apiKey = "644b946eec0b49f4ba12589738e0877f";
-
-  // const today = Date.now();
-  // useEffect(() => {
-  //   setLoading(true);
-  //   const fetchData = async () => {
-  //     const res = await axios.get(
-  //       `https://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=popularity&apiKey=644b946eec0b49f4ba12589738e0877f`
-  //     );
-
-  //     setArticles(res.data.articles);
-  //   };
-  //   fetchData();
-  // }, []);
-
-  const URL = "https://miguel-news-server.herokuapp.com/";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -43,7 +25,14 @@ const CryptoNews = () => {
 
   return (
     <Container maxWidth="md" className="news-items">
-      {<Typography variant="h2">Explore Crypto News!</Typography>}
+      {
+        <Typography
+          variant="h2"
+          style={{ fontFamily: "Montserrat", fontWeight: "bold" }}
+        >
+          Explore Crypto News!
+        </Typography>
+      }
       {articles
         .slice((page - 1) * 10, (page - 1) * 10 + 10)
         .map(({ title, description, url, urlToImage }) => {
