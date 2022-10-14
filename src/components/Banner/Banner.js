d939b85d689facd9a59111f2cff9964165cb5fc4
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-10-13 19:21:42 -0700

    added fade in effect

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,28 @@
 import { Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import Carousel from "./Carousel";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Banner = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
   return (
     <div>
       <Container className="bannerContent">
         <div className="tagline">
           <Typography
-            variant="h2"
+            data-aos="fade"
+            variant="h1"
             style={{
               fontWeight: "bold",
-              marginBottom: 15,
+              marginBottom: "20px",
+              marginTop: "30px",
               fontFamily: "Montserrat",
+              textShadow: "0.02em 0.05em #39f",
             }}
           >
             Pocket Crypto
